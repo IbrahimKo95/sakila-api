3286bc525c5f0cf3c5ff869aa466fe5a468d3b84
IbrahimKo95 <ibrahkonate95@gmail.com>
2024-12-06 15:43:21 +0100

    fix: remove unused routes

--- src/rental/rental.controller.ts
+++ src/rental/rental.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body} from '@nestjs/common';
 import { RentalService } from './rental.service';
 import { CreateRentalDto } from './dto/create-rental.dto';
-import { UpdateRentalDto } from './dto/update-rental.dto';
 
 @Controller('rental')
 export class RentalController {
@@ -10,25 +9,5 @@ export class RentalController {
   @Post()
   create(@Body() createRentalDto: CreateRentalDto) {
     return this.rentalService.create(createRentalDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.rentalService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rentalService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRentalDto: UpdateRentalDto) {
-    return this.rentalService.update(+id, updateRentalDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rentalService.remove(+id);
   }
 }
